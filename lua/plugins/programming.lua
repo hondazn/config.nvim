745e70bfd963cf184c8cb814b010c1e5d059c657
HONDA Zyun <hondazn.dev@gmail.com>
2025-02-15 14:59:45 +0900

    update

--- lua/plugins/programming.lua
+++ lua/plugins/programming.lua
@@ -76,7 +76,15 @@ return {
 		lazy = true,
 		event = { "BufReadPost", "BufNewFile" },
 		opts = {
-			formatters_by_ft = { lua = { "stylua" } },
+			formatters_by_ft = {
+				lua = { "stylua" },
+				typescript = { "biome-check", stop_after_first = true },
+				javascript = { "biome-check", stop_after_first = true },
+				typescriptreact = { "biome-check", stop_after_first = true },
+				javascriptreact = { "biome-check", stop_after_first = true },
+				json = { "biome" },
+				jsonc = { "biome" },
+			},
 		},
 	},
 	{
@@ -87,7 +95,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("config.treesitter")
-		end
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
