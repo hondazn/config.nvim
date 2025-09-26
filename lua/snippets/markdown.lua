2a4c62558a17b694df75dd598ff4e0c22db36eb9
hondazn <hondazn.dev@gmail.com>
2025-09-27 08:24:57 +0900

    comment out

--- lua/snippets/markdown.lua
+++ lua/snippets/markdown.lua
@@ -1,7 +1,7 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
-local f = ls.function_node
+-- local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
