231b7375da39d3ecfbc57d22c8a2e1ca5fa26fe4
hondazn <hondazn.dev@gmail.com>
2025-09-28 08:50:48 +0900

    change dashboard component order

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,8 +13,8 @@ return {
 			width = 80,
 			sections = {
 				{ header = vim.fn.system("bash -c '~/.config/nvim/scripts/kakugen.bash'") },
-				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
 				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
 				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
 				{ section = "startup" },
 			},
