7651096ebb26469114a42dc568b9adba5520da9b
hondazn <hondazn.dev@gmail.com>
2025-09-27 21:17:44 +0900

    add neovide effect

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -37,10 +37,10 @@ g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
-opt.helplang = 'ja'
+opt.helplang = "ja"
 opt.shortmess:append("sI")
 opt.scrolloff = 16
 
 -- for GUI
 o.guifont = "PlemolJP Console NF:h14"
-g.neovide_cursor_vfx_mode = "sonicboom"
+g.neovide_cursor_vfx_mode = { "sonicboom", "ripple", "torpedo", "pixiedust", "railgun" }
