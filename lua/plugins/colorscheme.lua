e01e69efa5714492fc8a1655fab8b4cf0d47cb46
hondazn <hondazn.dev@gmail.com>
2025-09-27 21:22:07 +0900

    update notification

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,21 +3,21 @@ return {
 		"EdenEast/nightfox.nvim",
 		lazy = false,
 		opts = {
-			groups = {
-				all = {
-					-- noice theme
-					-- NoiceCmdlinePrompt = { bg = "bg3" },
-					NoiceCmdlinePopup = { bg = "bg3" },
-					NoiceCmdlinePopupBorder = { bg = "bg3", fg = "bg3" },
-					-- NoiceCmdlinePopupTitle = { bg = "bg3", fg = "fg3" },
-					NoiceCmdlinePopupTitleCmdline = { bg = "bg3", fg = "fg3" },
-					NoiceCmdlinePopupTitleHelp = { bg = "bg3", fg = "fg3" },
-					NoiceCmdlinePopupTitleSearch = { bg = "bg3", fg = "fg3" },
-					NoiceCmdlinePopupTitleCalculator = { bg = "bg3", fg = "fg3" },
-					NoiceCmdlinePopupTitleInput = { bg = "bg3", fg = "fg3" },
-					NoiceCmdlinePopupTitleFilter = { bg = "bg3", fg = "fg3" },
-				},
-			},
+			-- groups = {
+			-- 	all = {
+			-- 		-- noice theme
+			-- 		-- NoiceCmdlinePrompt = { bg = "bg3" },
+			-- 		NoiceCmdlinePopup = { bg = "bg3" },
+			-- 		NoiceCmdlinePopupBorder = { bg = "bg3", fg = "bg3" },
+			-- 		-- NoiceCmdlinePopupTitle = { bg = "bg3", fg = "fg3" },
+			-- 		NoiceCmdlinePopupTitleCmdline = { bg = "bg3", fg = "fg3" },
+			-- 		NoiceCmdlinePopupTitleHelp = { bg = "bg3", fg = "fg3" },
+			-- 		NoiceCmdlinePopupTitleSearch = { bg = "bg3", fg = "fg3" },
+			-- 		NoiceCmdlinePopupTitleCalculator = { bg = "bg3", fg = "fg3" },
+			-- 		NoiceCmdlinePopupTitleInput = { bg = "bg3", fg = "fg3" },
+			-- 		NoiceCmdlinePopupTitleFilter = { bg = "bg3", fg = "fg3" },
+			-- 	},
+			-- },
 		},
 	},
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -35,6 +35,7 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
+		"folke/snacks.nvim"
 	},
 }
