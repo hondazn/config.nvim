dd660d554d3228b71c83f4fe52d6bdf899e98347
hondazn <hondazn.dev@gmail.com>
2025-10-01 00:54:25 +0900

    smear cursor

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,6 +1,6 @@
 return {
 	"sphamba/smear-cursor.nvim",
-	cond = not vim.g.neovide,
+	cond = not vim.g.neovide and not os.getenv("TERM") == "xterm-ghostty",
 	lazy = true,
 	event = "VeryLazy",
 	opts = {},
